2dc5676a04880f5609f9be0a0db57cf3a5350e56
bax <bakir.becic@fet.ba>
2025-12-18 16:49:30 +0100

    init_mod

--- kern/init.c
+++ kern/init.c
@@ -30,7 +30,6 @@ i386_init(void)
 	// Lab 2 memory management initialization functions
 	mem_init();
 
-<<<<<<< HEAD
 	// Lab 3 user environment initialization functions
 	env_init();
 	trap_init();
@@ -45,20 +44,9 @@ i386_init(void)
 
 	// We only have one user environment for now, so just run it.
 	env_run(&envs[0]);
-=======
-  int x = 1, y = 3, z = 4;
-  cprintf("x %d, y %x, z %d\n", x, y, z);
  
 	 
 
-  unsigned int i = 0x00646c72;
-  cprintf("H%x Wo%s", 57616, &i);
-
-  cprintf("x=%d y=%d", 3);
-	// Drop into the kernel monitor.
-	while (1)
-		monitor(NULL);
->>>>>>> lab2
 }
 
 
